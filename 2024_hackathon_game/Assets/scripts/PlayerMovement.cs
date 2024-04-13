d98d600a62eae8a40e739ab212459e14faf1454d
Clark Mattoon <mattoonc166@strose.edu>
2024-04-13 16:45:48 -0400

    player lane change is smooth now

--- 2024_hackathon_game/Assets/scripts/PlayerMovement.cs
+++ 2024_hackathon_game/Assets/scripts/PlayerMovement.cs
@@ -2,27 +2,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float lane_change_speed = 10;
+    private float seconds_to_lane_change = .1f;
     private GameObject player;
+    private Transform tran;
+    private Vector3 delta_vec;
+    private Vector3 pos_when_pressed;
+    private Vector3 goal_when_pressed;
+    private float timer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("player");
+        tran = player.transform;
+        delta_vec = new Vector3(3, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D) & player.transform.position.x < 6.5)
+        if (timer > 0)
         {
-            player.transform.position += new Vector3(3, 0, 0);
+            float perc = timer / seconds_to_lane_change;
+            timer += Time.deltaTime;
+            if (perc > 1)
+            {
+                perc = 1f;
+                timer = 0f;
+                tran.position = goal_when_pressed;
+            }
+            else
+            {
+                tran.position = Vector3.Lerp(pos_when_pressed, goal_when_pressed, perc);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.A) & player.transform.position.x > 2.5)
+        if (timer == 0 & Input.GetKeyDown(KeyCode.D) & player.transform.position.x < 7.5)
         {
-            player.transform.position -= new Vector3(3, 0, 0);
+            pos_when_pressed = tran.position;
+            goal_when_pressed = tran.position + delta_vec;
+            timer += Time.deltaTime;
+        }
+
+        if (timer == 0 & Input.GetKeyDown(KeyCode.A) & player.transform.position.x > 1.5)
+        {
+            pos_when_pressed = tran.position;
+            goal_when_pressed = tran.position - delta_vec;
+            timer += Time.deltaTime;
         }
     }
 }
