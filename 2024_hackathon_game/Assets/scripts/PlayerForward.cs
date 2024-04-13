e47c05ba8e178da9fb83135eaa3ccff000b73f83
Enea Zguro <126136264+P3rs0nal@users.noreply.github.com>
2024-04-13 18:37:18 -0400

    Professor spawning (as Eva's temp)

--- 2024_hackathon_game/Assets/scripts/PlayerForward.cs
+++ 2024_hackathon_game/Assets/scripts/PlayerForward.cs
@@ -8,7 +8,8 @@ public class PlayerForward : MonoBehaviour
     public float acceleration = 1.05f;
     public Transform Player;
     private float timer = 10f;
-    private float prev = 0;
+    private float spawntimer = 2f;
+    public GameObject Eva;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +20,13 @@ public class PlayerForward : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
+        spawntimer -= Time.deltaTime;
+        if (spawntimer < 0) { 
+            SpawnObject();
+            spawntimer = 2;
+        }
         if (timer < 0) { 
-            speed = speed*acceleration;
+            speed *= acceleration;
             speed = Mathf.Min(speed, 20f);
             timer = 10;
         }
@@ -28,4 +34,28 @@ public class PlayerForward : MonoBehaviour
         zPosUpdate += speed * Time.deltaTime;
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, zPosUpdate);
     }
+
+    void SpawnObject()
+    {
+        if (Eva != null)
+        {
+            Vector3 spawnDirection = transform.forward;
+
+            Vector3 spawnPosition = transform.position + spawnDirection * 10;
+
+            spawnPosition += spawnDirection * 2f;
+
+            spawnPosition.y = transform.position.y;
+
+            int x = Random.Range(1,4);
+
+            x *= 2;
+            x -= 1;
+
+            spawnPosition.x = transform.position.x-x;
+
+            Instantiate(Eva, spawnPosition, Quaternion.identity);
+        }
+    }
+
 }
