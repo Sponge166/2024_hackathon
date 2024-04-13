6d321db6ea6f54c0dce20f767f27a53cf0b55032
Clark Mattoon <mattoonc166@strose.edu>
2024-04-13 18:08:55 -0400

    made camera movement not follow player exactly

--- 2024_hackathon_game/Assets/scripts/PlayerMovement.cs
+++ 2024_hackathon_game/Assets/scripts/PlayerMovement.cs
@@ -4,18 +4,25 @@ public class PlayerMovement : MonoBehaviour
 {
     private float seconds_to_lane_change = .1f;
     private GameObject player;
-    private Transform tran;
+    private GameObject cam;
+    private Transform ptran;
+    private Transform ctran;
     private Vector3 delta_vec;
-    private Vector3 pos_when_pressed;
-    private Vector3 goal_when_pressed;
+    private Vector3 ppos_when_pressed;
+    private Vector3 pgoal_when_pressed;
+    private Vector3 cpos_when_pressed;
+    private Vector3 cgoal_when_pressed;
     private float timer = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("player");
-        tran = player.transform;
+        ptran = player.transform;
         delta_vec = new Vector3(3, 0, 0);
+        cam = GameObject.Find("3rd_person_cam");
+        ctran = cam.transform;
     }
 
     // Update is called once per frame
@@ -27,27 +34,36 @@ public class PlayerMovement : MonoBehaviour
             timer += Time.deltaTime;
             if (perc > 1)
             {
-                perc = 1f;
                 timer = 0f;
-                tran.position = goal_when_pressed;
+                ptran.position = pgoal_when_pressed;
+                ctran.position = cgoal_when_pressed;
             }
             else
             {
-                tran.position = Vector3.Lerp(pos_when_pressed, goal_when_pressed, perc);
+                ptran.position = Vector3.Lerp(ppos_when_pressed, pgoal_when_pressed, perc);
+                ctran.position = Vector3.Lerp(cpos_when_pressed, cgoal_when_pressed, perc);
             }
         }
 
-        if (timer == 0 & Input.GetKeyDown(KeyCode.D) & player.transform.position.x < 7.5)
+        if (timer == 0 & Input.GetKeyDown(KeyCode.D) & ptran.position.x < 7.5)
         {
-            pos_when_pressed = tran.position;
-            goal_when_pressed = tran.position + delta_vec;
+            ppos_when_pressed = ptran.position;
+            pgoal_when_pressed = ptran.position + delta_vec;
+
+            cpos_when_pressed = ctran.position;
+            cgoal_when_pressed = cpos_when_pressed + delta_vec / 2;
+
             timer += Time.deltaTime;
         }
 
-        if (timer == 0 & Input.GetKeyDown(KeyCode.A) & player.transform.position.x > 1.5)
+        if (timer == 0 & Input.GetKeyDown(KeyCode.A) & ptran.position.x > 1.5)
         {
-            pos_when_pressed = tran.position;
-            goal_when_pressed = tran.position - delta_vec;
+            ppos_when_pressed = ptran.position;
+            pgoal_when_pressed = ptran.position - delta_vec;
+
+            cpos_when_pressed = ctran.position;
+            cgoal_when_pressed = cpos_when_pressed - delta_vec / 2;
+
             timer += Time.deltaTime;
         }
     }
