3c1ae5e4cd21f4035140e7902054c56aeff10b21
rachelazc <142933662+rachelazc@users.noreply.github.com>
2024-04-14 12:52:23 -0400

    Added game music

---
+++ 2024_hackathon_game/Assets/scripts/AudioPlay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    AudioSource aud;
+
+    void Start() {
+        aud.GetComponent<AudioSource>();
+        aud.Play();
+    }
+    void Update() {
+
+    }
+}
